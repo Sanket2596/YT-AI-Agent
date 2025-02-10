9cadc4cc57b5fafa8604a2b9ba0ce345b8fe5a1b
Sanket2596 <48553092+Sanket2596@users.noreply.github.com>
2025-02-10 14:54:55 -0800

    added prompt caching for System, human and second-to-last human messages

--- youtube-ai-agent/lib/langgraph.ts
+++ youtube-ai-agent/lib/langgraph.ts
@@ -3,6 +3,7 @@ import { ToolNode } from "@langchain/langgraph/prebuilt";
 import wxflows from "@wxflows/sdk/langchain";
 import {
     END,
+    MemorySaver,
     MessagesAnnotation,
     START,
     StateGraph,
@@ -14,7 +15,19 @@ import {
 } from "@langchain/core/prompts";
 
 import SYSTEM_MESSAGE from "@/constants/systemMessage";
-import { SystemMessage } from "@langchain/core/messages";
+import { AIMessage, BaseMessage, HumanMessage, SystemMessage, trimMessages } from "@langchain/core/messages";
+
+
+// Trim the messages to manage conversation history
+const trimmer = trimMessages({
+    maxTokens: 10,
+    strategy: "last",
+    tokenCounter: (msgs) => msgs.length,
+    includeSystem: true,
+    allowPartial: true,
+    startOn: "human",
+});
+
 
 // Setting up tools to connect with our database and get those yout-tube transcripts extracted from our tools
 // Connecting to Tools
@@ -67,6 +80,24 @@ export const initialiseModel = () => {
     return model;
 };
 
+function shouldContinue(state: typeof MessagesAnnotation.State) {
+  const messages = state.messages;
+  const lastMessage = messages[messages.length - 1] as AIMessage;
+
+  // If the LLM makes a tool call, then we route to the "tools" node
+  if (lastMessage.tool_calls?.length) {
+    return "tools";
+  }
+
+  // If the last message is a tool message, route back to agent
+  if (lastMessage.content && lastMessage._getType() === "tool") {
+    return "agent";
+  }
+
+  // Otherwise, we stop (reply to the user)
+  return END;
+}
+
 const createWorkflow = () => {
     const model = initialiseModel();
     // StateGraph is a graph of flow for our LLM's -> it's going to create mainly a memory for us
@@ -80,7 +111,9 @@ const createWorkflow = () => {
             // Create Prompt Template with system message and message placeholder
             // The system message (SYSTEM_MESSAGE) serves as a predefined template or guideline for the agent's behavior. 
             // It can include instructions, rules, or context that the agent should follow when interacting with users.
-            //System messages are useful for setting the tone, context, and constraints for the agent's responses, ensuring consistency and adherence to specific guidelines.
+            // System messages are useful for setting the tone, context, and constraints for the agent's responses, ensuring consistency and adherence to specific guidelines.
+            // Prompt templates are a concept in LangChain designed to assist with this transformation -> transforming the raw user input into a list of messages that can be passed into a language model.
+            // They take in raw user input and return data (a prompt) that is ready to pass into a language model.
             const promptTemplate = ChatPromptTemplate.fromMessages([
                 new SystemMessage(systemContent, {
                     cache_control: { type: "ephemeral" } // Set a cache break point for the system message
@@ -88,8 +121,93 @@ const createWorkflow = () => {
                 new MessagesPlaceholder("message"),
             ]);
 
+            // Trim the messages to manage conversation history
+            const trimmedMessages = await trimmer.invoke(state.messages);
+
+            // Format the prompt message with the current message
+            const prompt = await promptTemplate.invoke({
+                messages: trimmedMessages,
+            });
+
+            // Get the message from the model and then this will invoke the chain of events of our messages
+            const response = await model.invoke(prompt);
+            // This will return Chunks of AI messages
+            return { response: [response] };
         }
+        // wheneever tools node is referred we're going to call the toolNode function in here
     )
-    
-    
+    .addEdge(START, "agent")
+    .addNode("tools", toolNode)
+    .addConditionalEdges("agent", shouldContinue)
+    .addEdge("tools", "agent");
+
+    return stateGraph;
 }
+
+function addCachingHeaders(messages: BaseMessage[]): BaseMessage[] {
+    // Rules of caching Headers for turn-by-turn conversations.
+    // The caching headers are used to manage the caching of messages in a conversation.
+    if (!messages.length) {
+        return messages;
+    }
+    // Create a copy of messages to avoid mutating the original copy of the message
+    const cachedMessages = [...messages];
+
+    // Helper to add Cache Control
+    function addCache (messages: BaseMessage) {
+        messages.content = [
+            {
+                type: "text",
+                text: messages.content as string,
+                cache_control: { type: "ephemeral" }, // Cache messages for 1 minute
+            }
+        ]
+    };
+
+    // Cache the last message
+    // console.log("Cached Messages", cachedMessages);
+    addCache(cachedMessages.at(-1)!);
+
+    // Find and cache the second-to-last Human message here
+    let humanCount = 0;
+    for (let i = cachedMessages.length - 1; i >= 0; i--) {
+        if (cachedMessages[i] instanceof HumanMessage) {
+            humanCount++;
+            if (humanCount === 2) {
+                addCache(cachedMessages[i]);
+                break;
+            }
+        }
+    }
+    
+        return cachedMessages;
+}
+
+
+export async function submitQuestion(messages: BaseMessage[], chatId: string) {
+
+    // Adding caching headers to messages
+    const cachedMessages = addCachingHeaders(messages);
+    console.log("Messsages", cachedMessages);
+
+    const workflow = createWorkflow();
+
+    // Creating a checkpoint to save the state of the conversation
+    const checkpointer = new MemorySaver();
+    const app = workflow.compile({ checkpointer });
+
+    // Run the graph now and stream the responses that we've got from hitting our model (Claude)
+    const stream = await app.streamEvents(
+        { 
+            messages: cachedMessages 
+
+        },
+        {
+          version: "v2",
+          configurable: { thread_id: chatId },
+          streamMode: "messages",
+          runId: chatId,
+        }
+      );
+      return stream; 
+};
